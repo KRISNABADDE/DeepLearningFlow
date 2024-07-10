df9ac4ea5aa4f20b28b98eb4dfdb837b45b57bda
KRISNABADDE <KRISNABADDE@gmail.com>
2024-07-10 10:26:05 +0530

    updeted

--- template.py
+++ template.py
@@ -24,7 +24,8 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
+    "templates/index.html",
+    ".env"
 ]
 
 for filepath in list_of_files:
